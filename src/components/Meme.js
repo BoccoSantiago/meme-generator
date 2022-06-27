306d78c43b36a6de4a1cbb4563cf588eed7c4a60
BoccoSantiago <italobocco@gmail.com>
2022-06-27 18:39:02 +0200

    first commit

---
+++ src/components/Meme.js
@@ -0,0 +1,45 @@
+import React from "react"
+import memesData from "../memesData.js"
+
+export default function Meme() {
+    const [meme, setMeme] = React.useState({
+        topText: '',
+        bottomText: '',
+        randomImage: "http://i.imgflip.com/1bij.jpg",
+    })
+    //const [allMemeImage, setAllMemeImage] = React.useState(memesData)
+    
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme, 
+            randomImage: url
+        }))
+    }
+    
+    return (
+        <main>
+            <div className="form">
+                <input 
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                />
+                <input 
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                />
+                <button 
+                    className="form--button"
+                    onClick={getMemeImage}
+                >
+                    Get a new meme image 🖼
+                </button>
+                <img className='memeImage' alt='memes.id' src={meme.randomImage}/>
+            </div>
+        </main>
+    )
+}
